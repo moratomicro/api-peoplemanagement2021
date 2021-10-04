1f81b17ed20579884b1522bbe8cc3e0d1443828f
moratomicro <moratomicro@yahoo.com.br>
2021-10-04 20:36:54 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/apipeoplemanagement2021/ApiPeoplemanagement2021Application.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.apipeoplemanagement2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPeoplemanagement2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPeoplemanagement2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/apipeoplemanagement2021/ApiPeoplemanagement2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.apipeoplemanagement2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPeoplemanagement2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
